ff4f7b1de734d7954013de86a949fe145333139f
ArktizML <arktiz.dev@gmail.com>
2025-12-27 20:53:15 +0100

    feat: add basic input validation for events

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,9 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from typing_extensions import Annotated
+
+Expectation = Annotated[int, Field(ge=0, le=100)]
+Reality = Annotated[int, Field(ge=0, le=300)]
+Title = Annotated[str, Field(min_length=3, max_length=100)]
 
 class EventCreate(BaseModel):
-    title: str
-    expectation: int
-    reality: int
+    title: Title
+    expectation: Expectation
+    reality: Reality
+
+    @field_validator("reality")
+    @classmethod
+    def reality_not_too_extreme(cls, reality, info):
+        expectation = info.data.get("expectation")
+
+        if expectation is not None and reality > expectation * 3:
+            raise ValueError("Reality is unrealistically higher than expectation.")
 
 class EventOut(BaseModel):
     id: int
