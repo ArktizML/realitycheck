1281300901d1d42284033d4a4063b4534a1bb52a
ArktizML <arktiz.dev@gmail.com>
2026-01-09 12:04:36 +0100

    feat: add edit button to card, added edit form

--- app/api/pages.py
+++ app/api/pages.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, Request, Form
+from fastapi import APIRouter, Depends, Request, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from starlette.status import HTTP_303_SEE_OTHER
 from app.database import get_db
-from app.services.event_service import get_all_events
+from app.services.event_service import get_all_events, get_event_by_id
 from app.schemas.event import EventCreate
 from app.services.event_service import create_event
 
@@ -36,3 +36,42 @@ def create_event_from_form(
 ):
     create_event(db=db, event=EventCreate(title=title, expectation=expectation, reality=reality))
     return RedirectResponse(url="/", status_code=HTTP_303_SEE_OTHER)
+
+@router.get("/events/{event_id}/edit", response_class=HTMLResponse)
+def edit_event_page(
+    request: Request,
+    event_id: int,
+    db: Session = Depends(get_db),
+):
+    event = get_event_by_id(db, event_id)
+    if not event:
+        raise HTTPException(status_code=404)
+
+    return templates.TemplateResponse(
+        "edit_event.html",
+        {
+            "request": request,
+            "event": event,
+        },
+    )
+
+@router.post("/events/{event_id}/edit")
+def update_event_from_form(
+    event_id: int,
+    title: str = Form(...),
+    expectation: int = Form(...),
+    reality: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    event = get_event_by_id(db, event_id)
+    if not event:
+        raise HTTPException(status_code=404)
+
+    event.title = title
+    event.expectation = expectation
+    event.reality = reality
+    event.gap = reality - expectation
+
+    db.commit()
+
+    return RedirectResponse("/", status_code=303)
